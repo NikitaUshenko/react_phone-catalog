d1d232bf7b2c59b8107b3e2a781a9ba91cd2c890
NikitaUshenko <nikita-ushenko@mail.ru>
2019-08-08 15:53:25 +0300

    Updates

---
+++ precache-manifest.81c2c1e6ebf5266e735964f8c5e0ddb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ec1a3e0098b0c783939d2e2f9423a2",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "2fd3197c35b7698cb317",
+    "url": "/react_phone-catalog/static/css/main.6b171aae.chunk.css"
+  },
+  {
+    "revision": "d3026b885a1dded381b4",
+    "url": "/react_phone-catalog/static/js/2.c0ac6bd3.chunk.js"
+  },
+  {
+    "revision": "2fd3197c35b7698cb317",
+    "url": "/react_phone-catalog/static/js/main.b055e014.chunk.js"
+  },
+  {
+    "revision": "9b3e1d68b9cc1dd9a925",
+    "url": "/react_phone-catalog/static/js/runtime~main.25bc40f7.js"
+  }
+]);
